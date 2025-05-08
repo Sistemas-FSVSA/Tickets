b9f1dd5c74c85dedd69f29d94e2d9d962057a2e1
Sistemas-FSVSA <sistemas@funerariasanvicente.com>
2025-05-08 13:56:11 -0500

    ajustes

--- BackEnd/routes/dashboard.js
+++ BackEnd/routes/dashboard.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
+const { obtenerSoportes } = require('../controllers/dashboard/obtenerSoportes');
+const { obtenerDependencias } = require('../controllers/dashboard/obtenerDependencias');
+
 const router = Router();
 
-const rateLimiterStrict = require('../models/rateLimiterStrict'); // 1 solicitud por minuto
-const rateLimiterFast = require('../models/rateLimiterFast'); // 100 solicitudes por segundo
-
-const { obtenerTicketsArea } = require('../controllers/dashboard/obtenerTicketsArea');
-
-const authenticateToken = require('../models/authMiddleware');
-
-// Usar el middleware de Multer antes del controlador
-router.post('/obtenerTicketsArea', authenticateToken, rateLimiterFast, obtenerTicketsArea);
-
+router.post('/obtenerSoportes', obtenerSoportes);
+router.post('/obtenerDependencias', obtenerDependencias);
 
 module.exports = router;
